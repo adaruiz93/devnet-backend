6e65c9c0473bd16b97174a5101d4ddc6ff5f99ce
ARI-FLOWERS <ARI.WEEDON@GMAIL.COM>
2021-10-04 13:31:52 -0700

    Create route functioning

--- controllers/devnet.js
+++ controllers/devnet.js
@@ -4,7 +4,7 @@ const Devnet = require('../models/devnet')
 
 
 //get routes
-//index
+//INDEX
 router.get('/', async (req,res) =>{
   try{
     const allDevs = await Devnet.find()
@@ -13,4 +13,17 @@ router.get('/', async (req,res) =>{
     res.status(400).json({ error: err.message })
   }
 })
+
+//CREATE / POST
+router.post('/', async (req, res) => {
+  try{
+    const newDev = await Devnet.create(req.body)
+    res.status(200).json(newDev)
+  } catch(err) {
+    res.status(400).json({ error: err.message })
+  }
+})
+
+
+
 module.exports = router
